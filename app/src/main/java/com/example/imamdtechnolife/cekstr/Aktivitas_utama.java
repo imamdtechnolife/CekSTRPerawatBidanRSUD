b48e7e0a47a2b41af45b7149ac0e9379ef26f6af
imamdtechnolife <imamdtechnolife@gmail.com>
2018-02-11 02:26:30 +0800

    update

--- app/src/main/java/com/example/imamdtechnolife/cekstr/Aktivitas_utama.java
+++ app/src/main/java/com/example/imamdtechnolife/cekstr/Aktivitas_utama.java
@@ -3,26 +3,30 @@ package com.example.imamdtechnolife.cekstr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Aktivitas_utama extends AppCompatActivity {
 
-    private Button btnSimpan;
-    private TextView txtNama;
-    private  TextView txtRuang;
+    private Button btnSignUp;
+    private TextView txtEmail;
+    private  TextView txtPassword;
     private  TextView txtTampil;
+    private ListView listNama;
+    private ArrayList<String> listNamaNama = new ArrayList<>();
+    private ArrayList<String> listKey = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +37,27 @@ public class Aktivitas_utama extends AppCompatActivity {
         {
 
             final DatabaseReference mdatabase = FirebaseDatabase.getInstance().getReference().child("Perawat");
-            btnSimpan = (Button) findViewById(R.id.btnSimpan);
-            txtNama = (TextView) findViewById(R.id.txtNama);
-            txtRuang = (TextView) findViewById(R.id.txtRuangan);
+            btnSignUp = (Button) findViewById(R.id.btnSignUp);
+            txtEmail = (TextView) findViewById(R.id.txtEmail);
+            txtPassword = (TextView) findViewById(R.id.txtPassword);
             final HashMap<String, String> dataMap = new HashMap<String, String>();
             txtTampil = (TextView) findViewById(R.id.txt_Tampil);
+            final ArrayAdapter<String> list = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listNamaNama);
+            listNama = (ListView) findViewById(R.id.ListNama);
+            listNama.setAdapter(list);
 
-            btnSimpan.setOnClickListener(new View.OnClickListener() {
+
+            btnSignUp.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
-                    String nama = txtNama.getText().toString().trim();
-                    String ruang = txtRuang.getText().toString().trim();
+                    String nama = txtEmail.getText().toString().trim();
+                    String ruang = txtPassword.getText().toString().trim();
 
                     dataMap.put("Nama", nama);
                     dataMap.put("Ruang", ruang);
 
-                    mdatabase.child("Perawat").setValue(dataMap);
+                    mdatabase.setValue(dataMap);
 
                 }
             });
@@ -60,6 +68,39 @@ public class Aktivitas_utama extends AppCompatActivity {
 
                     String ini = dataSnapshot.getValue().toString();
                     txtTampil.setText("Nama" + ini);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            mdatabase.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                    String isi = dataSnapshot.getValue(String.class);
+                    listNamaNama.add(isi);
+                    String key = dataSnapshot.getKey();
+                    listKey.add(key);
+                    list.notifyDataSetChanged();
+
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
                 }
 
                 @Override
